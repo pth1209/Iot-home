66aa62c3543ee2bd70e9f2af9130b9238a97ddc0
pth1209 <lawrencepark1@gmail.com>
2022-10-18 20:32:29 -0500

    Initial Commit

---
+++ morning-routine Watch App/morning_routineApp.swift
@@ -0,0 +1,17 @@
+//
+//  morning_routineApp.swift
+//  morning-routine Watch App
+//
+//  Created by Lawrence Park on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct morning_routine_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ morning-routine/morning_routineApp.swift
@@ -0,0 +1,17 @@
+//
+//  morning_routineApp.swift
+//  morning-routine
+//
+//  Created by Lawrence Park on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct morning_routineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

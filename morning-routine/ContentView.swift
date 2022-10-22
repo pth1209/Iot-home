9c64a4820da0d6f55cd5663e87e31ce0da5fd446
pth1209 <lawrencepark1@gmail.com>
2022-10-22 11:51:23 -0500

    First commit

--- morning-routine/ContentView.swift
+++ morning-routine/ContentView.swift
@@ -6,18 +6,30 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    private var healthStore: HealthStore?
+    
+    init() {
+        healthStore = HealthStore()
     }
-}
+    
+    var body: some View {
+        Text("I hate swift").padding()
+        
+            .onAppear{
+                if let healthStore = healthStore {
+                    healthStore.requestAuthorization {success in}
+                }
+            }
+        
+     
+                }
+            }
+                
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ morning-routine/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  morning-routine
+//
+//  Created by Lawrence Park on 10/22/22.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
